72f6ad31ba47c5f031f30d4a7b27d4f4597d9956
baiching <bachmarma@gmail.com>
2020-05-16 12:20:42 +0600

    add comments and modifications in Gaussian filter

--- gaussian_filter.py
+++ gaussian_filter.py
@@ -4,7 +4,19 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-def gaussian_filter(in_channel, out_channel, channels=3, kernel_size=15, sigma=3):
+def gaussian_filter(in_channel, out_channel, kernel_size=15, sigma=3):
+    """
+    This method returns 2d gaussian filter
+
+    input :
+    in_channel : Number of input channels
+    out_channel : Expected number of output channels
+    kernel_size : size of the filter (H x H)
+    sigma : sigma
+
+    output:
+    returns : gaussian_filter
+    """
     # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
     x_cord = torch.arange(kernel_size)
     x_grid = x_cord.repeat(kernel_size).view(kernel_size, kernel_size)
@@ -27,10 +39,10 @@ def gaussian_filter(in_channel, out_channel, channels=3, kernel_size=15, sigma=3
 
     # Reshape to 2d depthwise convolutional weight
     gaussian_kernel = gaussian_kernel.view(1, 1, kernel_size, kernel_size)
-    gaussian_kernel = gaussian_kernel.repeat(channels, 1, 1, 1)
+    gaussian_kernel = gaussian_kernel.repeat(in_channel, 1, 1, 1)
 
     gaussian_filter = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
-                            kernel_size=kernel_size, groups=channels, bias=False)
+                            kernel_size=kernel_size, groups=in_channel, bias=False)
 
     gaussian_filter.weight.data = gaussian_kernel
     gaussian_filter.weight.requires_grad = False

6c4f0bcb0e23b443bf1ea7a4a3aa26e7ca5b5030
baiching <bachmarma@gmail.com>
2020-05-16 12:12:58 +0600

    added gaussian 2d filtering

---
+++ gaussian_filter.py
@@ -0,0 +1,38 @@
+import math
+import numbers
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+def gaussian_filter(in_channel, out_channel, channels=3, kernel_size=15, sigma=3):
+    # Create a x, y coordinate grid of shape (kernel_size, kernel_size, 2)
+    x_cord = torch.arange(kernel_size)
+    x_grid = x_cord.repeat(kernel_size).view(kernel_size, kernel_size)
+    y_grid = x_grid.t()
+    xy_grid = torch.stack([x_grid, y_grid], dim=-1)
+
+    mean = (kernel_size - 1)/2.
+    variance = sigma**2.
+
+    # Calculate the 2-dimensional gaussian kernel which is
+    # the product of two gaussian distributions for two different
+    # variables (in this case called x and y)
+    gaussian_kernel = (1./(2.*math.pi*variance)) *\
+                  torch.exp(
+                      -torch.sum((xy_grid - mean)**2., dim=-1) /\
+                      (2*variance)
+                  )
+    # Make sure sum of values in gaussian kernel equals 1.
+    gaussian_kernel = gaussian_kernel / torch.sum(gaussian_kernel)
+
+    # Reshape to 2d depthwise convolutional weight
+    gaussian_kernel = gaussian_kernel.view(1, 1, kernel_size, kernel_size)
+    gaussian_kernel = gaussian_kernel.repeat(channels, 1, 1, 1)
+
+    gaussian_filter = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                            kernel_size=kernel_size, groups=channels, bias=False)
+
+    gaussian_filter.weight.data = gaussian_kernel
+    gaussian_filter.weight.requires_grad = False
+
+    return gaussian_filter
